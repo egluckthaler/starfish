0915331ea0eb0126fc8fb74a44ffd075765f4637
egluckthaler <emilegluckthaler@gmail.com>
2023-12-17 13:04:48 -0600

    update organization

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
-mkdir -p $PREFIX/commands $PREFIX/aux $PREFIX/bin $PREFIX/db $PREFIX/lib
+mkdir -p $PREFIX/main $PREFIX/aux $PREFIX/bin $PREFIX/db $PREFIX/lib
 cp -r aux/* $PREFIX/aux/
-cp -r commands/* $PREFIX/commands/
+cp -r main/* $PREFIX/main/
 cp -r bin/* $PREFIX/bin/
 cp -r db/* $PREFIX/db/
 cp -r lib/* $PREFIX/lib/
