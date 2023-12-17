eecfdd173ee2db1ecd67360a8a92b3bc52d26007
egluckthaler <emilegluckthaler@gmail.com>
2023-12-16 18:55:53 -0600

    update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,6 @@
-mkdir -p $PREFIX/data
-cp -r data_dir/* $PREFIX/data/
+mkdir -p $PREFIX/exe $PREFIX/aux $PREFIX/bin $PREFIX/db $PREFIX/lib
+cp -r aux/* $PREFIX/aux/
+cp -r exe/* $PREFIX/exe/
+cp -r bin/* $PREFIX/bin/
+cp -r db/* $PREFIX/db/
+cp -r lib/* $PREFIX/lib/
