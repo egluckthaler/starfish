840f9943462cf4a3158cf7251adb0ab61a4e95ce
egluckthaler <emilegluckthaler@gmail.com>
2023-12-18 06:54:44 -0600

    remove CNEFinder libs from build

--- build.sh
+++ build.sh
@@ -8,10 +8,9 @@ mv cnef ../bin/
 
 # copy git directory structure to conda package location
 cd $SRC_DIR
-mkdir -p $PREFIX/main $PREFIX/aux $PREFIX/bin $PREFIX/db $PREFIX/lib $PREFIX/CNEFinder
+mkdir -p $PREFIX/main $PREFIX/aux $PREFIX/bin $PREFIX/db $PREFIX/lib
 cp -r aux/* $PREFIX/aux/
 cp -r main/* $PREFIX/main/
 cp -r bin/* $PREFIX/bin/
 cp -r db/* $PREFIX/db/
 cp -r lib/* $PREFIX/lib/
-cp -r CNEFinder/* $PREFIX/CNEFinder/
